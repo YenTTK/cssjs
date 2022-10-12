513f0ea40bf925a5758a1a9bc5927e49a46c4f6b
YenTTK <115631875+YenTTK@users.noreply.github.com>
2022-10-12 21:04:48 +0700

    Update redirectcopy code chống copy.js

--- redirectcopy code chống copy.js
+++ redirectcopy code chống copy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://faceseo.vn'>http://faceseo.vn</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://kimyen1509.blogspot.com/'>https://kimyen1509.blogspot.com/</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
